e75a6aa9f334f225cd901b381122cfc985335ce0
jayendra <dimebeatengreen8@gmail.com>
2021-08-19 19:24:41 +0530

    Updates

---
+++ precache-manifest.9b220ae568c6f84c145a12ec7ffdc753.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450b094a7be5842f07eedc510cc9256c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ac8d2c8e5e9e7667e1b4",
+    "url": "/portfolio/static/css/main.7c2b395a.chunk.css"
+  },
+  {
+    "revision": "9d3fa793c38f2529736e",
+    "url": "/portfolio/static/js/2.b4a90e14.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/portfolio/static/js/2.b4a90e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8d2c8e5e9e7667e1b4",
+    "url": "/portfolio/static/js/main.bbda5896.chunk.js"
+  },
+  {
+    "revision": "0e5bab216ddc30a34654",
+    "url": "/portfolio/static/js/runtime-main.b7decbf4.js"
+  },
+  {
+    "revision": "ae3f566eae9a39f8a4fac19278e7b90a",
+    "url": "/portfolio/static/media/Screenshot 2021-08-18 194059.ae3f566e.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/portfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "e75b14a597d878e422f5a5b32ec020d5",
+    "url": "/portfolio/static/media/feextjoaqkf41.e75b14a5.png"
+  },
+  {
+    "revision": "7ed74b9293ab048e3b1c17113a90ee82",
+    "url": "/portfolio/static/media/first.7ed74b92.jpg"
+  },
+  {
+    "revision": "cb478921009528abb858cefa6f834f7c",
+    "url": "/portfolio/static/media/frt.cb478921.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/portfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/portfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/portfolio/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "df7bd3116fa765bcea495aa0d82468b9",
+    "url": "/portfolio/static/media/spc.df7bd311.jpg"
+  },
+  {
+    "revision": "3253b59bc8bf61d71f902448ad8516d7",
+    "url": "/portfolio/static/media/trd.3253b59b.jpg"
+  },
+  {
+    "revision": "5bcdb145b3b60dc6328e718aa1d37779",
+    "url": "/portfolio/static/media/webscr.5bcdb145.png"
+  }
+]);
