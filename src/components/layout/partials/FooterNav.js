e7869e8567c09b06894d14e1e40e0408fa54c38e
jayendra <dimebeatengreen8@gmail.com>
2021-08-19 18:50:08 +0530

    first commit

---
+++ src/components/layout/partials/FooterNav.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+
+const FooterNav = ({ className, ...props }) => {
+  const classes = classNames("footer-nav", className);
+
+  return <nav></nav>;
+};
+
+export default FooterNav;
